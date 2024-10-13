da3fdd2b97e245df715a79c685b10d86b43becb7
geraldosaidina <gerarantes99@gmail.com>
2024-10-13 23:30:16 +0200

    models creation

--- src/main/java/com/gulera/gulera_backend/models/User.java
+++ src/main/java/com/gulera/gulera_backend/models/User.java
@@ -1,4 +1,35 @@
 package com.gulera.gulera_backend.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String user_id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Workout> workouts = new ArrayList<>();
+
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    private Date updatedAt;
+
 }
